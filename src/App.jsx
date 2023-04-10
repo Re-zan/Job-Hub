bfa2be2844760d002a761d295d96537a92bd5fea
Re-zan <zannat881all@gmail.com>
2023-04-10 12:10:36 +0600

    router installl done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline bg-slate-900">
+      <h1 className="text-3xl font-bold underline bg-slate-900 clifford text-[90deg, #7E90FE 0%, #9873FF 100%]">
         Hello world!
       </h1>
     </div>
