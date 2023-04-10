ac1b34d800be40624bbe33985dbe6be7cd9e96ef
Re-zan <zannat881all@gmail.com>
2023-04-10 15:43:29 +0600

    navbar done in responsivly

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="md:container sm:conatiner mx-auto justify-between items-center md:flex pt-12 pb-10">
+    <nav className="sm:max-w-xl md:max-w-full lg:max-w-screen-xl mx-auto justify-between items-center  pt-12 pb-10 md:flex">
       <Link to="/">
-        <div className=" text-[#1A1919]  text-4xl md:text-base lg:text-3xl font-bold pl-4 md:pl-0 pb-3 md:pb-0">
-          JOB-HUB
+        <div className=" text-[#1A1919]  text-3xl md:text-base lg:text-3xl font-bold pl-4 md:pl-0 pb-3 md:pb-0 flex justify-between">
+          <h2>JOB-HUB</h2>
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <span>
+              {isOpen === true ? (
+                <XMarkIcon className="h-6 w-6  text-[#9873FF] md:hidden mr-3" />
+              ) : (
+                <Bars3Icon className="h-6 w-6  text-[#9873FF] md:hidden mr-3" />
+              )}
+            </span>
+          </div>
         </div>
       </Link>
 
-      <ul className="md:flex px-6 items-center ">
+      <ul
+        className={` md:flex px-6 items-center absolute md:static transition ease-in-out duration-500 ${
+          isOpen
+            ? " top-24 z-10  bg-[#acb4e8] text-white text-center  w-full py-7"
+            : " -top-56 "
+        }`}
+      >
         <NavLink
           to="/"
           className={({ isActive }) =>
@@ -54,8 +70,16 @@ const Header = () => {
         >
           <li className="py-3 md:px-7">Blog</li>
         </NavLink>
+
+        <button
+          className={`md:hidden bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white py-5 px-7  rounded-lg ${
+            isOpen ? " " : " hidden"
+          }`}
+        >
+          Start Applying
+        </button>
       </ul>
-      <button className=" bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white py-5 px-7 rounded-lg">
+      <button className=" bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white py-5 px-7  rounded-lg hidden md:block">
         Start Applying
       </button>
     </nav>
