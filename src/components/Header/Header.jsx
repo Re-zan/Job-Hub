a2b94aa2f8250b520efb2f1cb016d0186a72f4b1
Re-zan <zannat881all@gmail.com>
2023-04-10 14:35:46 +0600

    navbar design done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,64 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h3>This header</h3>
-    </div>
+    <nav className="md:container sm:conatiner mx-auto justify-between items-center md:flex pt-12 pb-10">
+      <Link to="/">
+        <div className=" text-[#1A1919]  text-4xl md:text-base lg:text-3xl font-bold pl-4 md:pl-0 pb-3 md:pb-0">
+          JOB-HUB
+        </div>
+      </Link>
+
+      <ul className="md:flex px-6 items-center ">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? " md:text-transparent bg-clip-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-base font-semibold "
+              : " text-[#757575]"
+          }
+        >
+          <li className="py-3 md:px-7 ">Home</li>
+        </NavLink>
+
+        <NavLink
+          to="/statistics"
+          className={({ isActive }) =>
+            isActive
+              ? " md:text-transparent bg-clip-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-base font-semibold "
+              : " text-[#757575]"
+          }
+        >
+          <li className="py-3 md:px-7">Statistics</li>
+        </NavLink>
+
+        <NavLink
+          to="/applied-jobs"
+          className={({ isActive }) =>
+            isActive
+              ? " md:text-transparent bg-clip-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-base font-semibold "
+              : " text-[#757575]"
+          }
+        >
+          <li className="py-3 md:px-7">Applied Jobs</li>
+        </NavLink>
+
+        <NavLink
+          to="/blog"
+          className={({ isActive }) =>
+            isActive
+              ? " md:text-transparent bg-clip-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-base font-semibold"
+              : " text-[#757575]"
+          }
+        >
+          <li className="py-3 md:px-7">Blog</li>
+        </NavLink>
+      </ul>
+      <button className=" bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white py-5 px-7 rounded-lg">
+        Start Applying
+      </button>
+    </nav>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./components/Home/Home";
+import Statistics from "./components/Statistics/Statistics";
+import Applied from "./components/Applied Jobs/Applied";
+import Blog from "./components/Blog/Blog";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "statistics",
+        element: <Statistics></Statistics>,
+      },
+      {
+        path: "applied-jobs",
+        element: <Applied></Applied>,
+      },
+      {
+        path: "blog",
+        element: <Blog></Blog>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
